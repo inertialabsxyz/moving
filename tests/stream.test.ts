ae6ceb9f60baabe2e1b25a087b23d20d499c0008
Andy Bell <andyjsbell@gmail.com>
2025-03-20 19:06:21 +0100

    refactor the accounts

--- tests/stream.test.ts
+++ tests/stream.test.ts
@@ -3,13 +3,18 @@ import {
     AptosConfig,
     Network,
     Account,
-    Ed25519Account,
+    Ed25519Account, MoveValue,
 } from "@aptos-labs/ts-sdk";
 import {deploy} from "./deploy";
 const config = new AptosConfig({ network: Network.LOCAL });
 const aptos = new Aptos(config);
-const alice = Account.generate();
-const bob = Account.generate();
+
+const accounts = {
+    alice: Account.generate(),
+    bob: Account.generate(),
+    charlie: Account.generate(),
+    dave: Account.generate()
+};
 const MINT_AMOUNT = 100_000_000;
 
 let deployerAccount = Account.generate();
@@ -30,9 +35,7 @@ async function migrateAccountToFA(signer: Ed25519Account) {
         },
     });
     const pendingTransaction = await aptos.signAndSubmitTransaction({signer, transaction});
-    const executedTransaction = await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
-
-    console.log(`Migration for account ${signer.accountAddress.toString()} with tx hash ${executedTransaction.hash}`);
+    await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
 }
 
 async function mintSTRM(signer: Ed25519Account, account: Ed25519Account) {
@@ -45,27 +48,26 @@ async function mintSTRM(signer: Ed25519Account, account: Ed25519Account) {
         },
     });
     const pendingTransaction = await aptos.signAndSubmitTransaction({signer, transaction});
-    const executedTransaction = await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
-
-    console.log(`Funded account ${account.accountAddress.toString()} with ${MINT_AMOUNT} STRM with tx hash ${executedTransaction.hash}`);
+    await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
 }
 
+async function setupAccounts(accounts: Array<Ed25519Account>) {
+    for (const account of accounts) {
+        await aptos.fundAccount({accountAddress: account.accountAddress, amount: MINT_AMOUNT});
+        await migrateAccountToFA(account);
+        await mintSTRM(deployerAccount, account);
+    }
+}
 beforeAll(async () => {
     deployerAccount = await deploy();
-    await aptos.fundAccount({accountAddress: alice.accountAddress, amount: MINT_AMOUNT});
-    await aptos.fundAccount({accountAddress: bob.accountAddress, amount: MINT_AMOUNT});
-    await migrateAccountToFA(alice);
-    await migrateAccountToFA(bob);
-    await mintSTRM(deployerAccount, alice);
-    await mintSTRM(deployerAccount, bob);
+    await setupAccounts(Object.values(accounts));
 }, 60000);
 
 interface WalletStore {
     inner: string
 }
 
-async function getPoolObject(account: Ed25519Account, token: string) {
-    // Verify pool and assets in store
+async function getPoolAddress(account: Ed25519Account, token: string) : Promise<MoveValue> {
     const result = await aptos.view({
         payload: {
             function: `${deployerAccount.accountAddress}::streams::pool_address`,
@@ -75,34 +77,51 @@ async function getPoolObject(account: Ed25519Account, token: string) {
     });
 
     expect(result.length).toBe(1);
-    const poolAddress = result[0]?.toString() || "";
+
+    return result[0]?.toString() || "";
+}
+
+async function getPoolObject(account: Ed25519Account, token: string) {
+    const poolAddress = await getPoolAddress(account, token);
     let type = `${deployerAccount.accountAddress}::streams::Pool<0x1::object::Object<0x1::fungible_asset::Metadata>>`;
 
     // @ts-ignore
     return await aptos.getAccountResource({accountAddress: poolAddress, resourceType: type});
 }
 
-// https://github.com/aptos-labs/aptos-ts-sdk/tree/main/examples/typescript
-test("Create a pool", async () => {
-    // aptos move run --function-id 17d1169c2f4e744ea21495510702cae3eba2230e928713bc07ae7c611cd1269b::streams::create_pool --type-args 0x1::fungible_asset::Metadata --args address:0xa u64:1000000
-    let oldBalance = await aptos.account.getAccountAPTAmount(alice);
-    let poolAmount = 100;
+async function createPool(account: Ed25519Account, poolAmount: number) {
     const transaction = await aptos.transaction.build.simple({
-        sender: alice.accountAddress,
+        sender: accounts.alice.accountAddress,
         data: {
             function: `${deployerAccount.accountAddress}::streams::create_pool`,
             functionArguments: [Tokens.APT, poolAmount],
             typeArguments: ["0x1::fungible_asset::Metadata"],
         },
     });
-    const pendingTransaction = await aptos.signAndSubmitTransaction({signer: alice, transaction});
+    const pendingTransaction = await aptos.signAndSubmitTransaction({signer: accounts.alice, transaction});
+    await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
+}
+
+// https://github.com/aptos-labs/aptos-ts-sdk/tree/main/examples/typescript
+test("Create a pool", async () => {
+    // aptos move run --function-id 17d1169c2f4e744ea21495510702cae3eba2230e928713bc07ae7c611cd1269b::streams::create_pool --type-args 0x1::fungible_asset::Metadata --args address:0xa u64:1000000
+    let oldBalance = await aptos.account.getAccountAPTAmount(accounts.alice);
+    let poolAmount = 100;
+    const transaction = await aptos.transaction.build.simple({
+        sender: accounts.alice.accountAddress,
+        data: {
+            function: `${deployerAccount.accountAddress}::streams::create_pool`,
+            functionArguments: [Tokens.APT, poolAmount],
+            typeArguments: ["0x1::fungible_asset::Metadata"],
+        },
+    });
+    const pendingTransaction = await aptos.signAndSubmitTransaction({signer: accounts.alice, transaction});
     const executedTransaction = await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
     const gasCost = parseInt(executedTransaction.gas_used) * (await aptos.getGasPriceEstimation()).gas_estimate;
-    console.log(`Pool created:${executedTransaction.hash}`);
     // Verify assets have left wallet
-    let newBalance = await aptos.account.getAccountAPTAmount(alice);
+    let newBalance = await aptos.account.getAccountAPTAmount(accounts.alice);
     expect(newBalance).toBe(oldBalance - poolAmount - gasCost);
-    const poolObject = await getPoolObject(alice, Tokens.APT);
+    const poolObject = await getPoolObject(accounts.alice, Tokens.APT);
     const availableStoreAddress = poolObject["available"]["store"]["inner"];
     const availableStore = await aptos.getAccountResource({accountAddress: availableStoreAddress, resourceType: "0x1::fungible_asset::FungibleStore"});
     expect(availableStore["balance"]).toBe("100");
@@ -111,7 +130,7 @@ test("Create a pool", async () => {
     const walletView = await aptos.view({
         payload: {
             function: "0x1::primary_fungible_store::primary_store",
-            functionArguments: [`${alice.accountAddress}`, Tokens.APT],
+            functionArguments: [`${accounts.alice.accountAddress}`, Tokens.APT],
             typeArguments: ["0x1::fungible_asset::Metadata"],
         }
     });
@@ -119,7 +138,7 @@ test("Create a pool", async () => {
         const inner = (walletView[0] as WalletStore).inner || "";
         // Verify that funds can't be manipulated
         const transferFA = await aptos.transaction.build.simple({
-            sender: alice.accountAddress,
+            sender: accounts.alice.accountAddress,
             data: {
                 function: "0x1::fungible_asset::transfer",
                 functionArguments: [availableStoreAddress, inner, 10],
@@ -127,7 +146,7 @@ test("Create a pool", async () => {
             },
         });
 
-        const pendingTransaction = await aptos.signAndSubmitTransaction({signer: alice, transaction: transferFA});
+        const pendingTransaction = await aptos.signAndSubmitTransaction({signer: accounts.alice, transaction: transferFA});
         await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
 
         throw new Error("Expected failure of transaction");
@@ -139,40 +158,29 @@ test("Create a pool", async () => {
 test("Drain from the pool", async () => {
     // Create the pool
     let poolAmount = 100;
-    {
-        const transaction = await aptos.transaction.build.simple({
-            sender: alice.accountAddress,
-            data: {
-                function: `${deployerAccount.accountAddress}::streams::create_pool`,
-                functionArguments: [Tokens.APT, poolAmount],
-                typeArguments: ["0x1::fungible_asset::Metadata"],
-            },
-        });
-        const pendingTransaction = await aptos.signAndSubmitTransaction({signer: alice, transaction});
-        await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
-    }
+    await createPool(accounts.alice, poolAmount);
 
     let drainAmount = 50;
-    let oldBalance = await aptos.account.getAccountAPTAmount(alice);
+    let oldBalance = await aptos.account.getAccountAPTAmount(accounts.alice);
     // Drain the pool
     const transaction = await aptos.transaction.build.simple({
-        sender: alice.accountAddress,
+        sender: accounts.alice.accountAddress,
         data:{
             function: `${deployerAccount.accountAddress}::streams::drain_pool`,
             functionArguments: [Tokens.APT, drainAmount],
             typeArguments: ["0x1::fungible_asset::Metadata"],
         }
     });
-    const pendingTransaction = await aptos.signAndSubmitTransaction({signer: alice, transaction});
+    const pendingTransaction = await aptos.signAndSubmitTransaction({signer: accounts.alice, transaction});
     const executedTransaction = await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
     const gasCost = parseInt(executedTransaction.gas_used) * (await aptos.getGasPriceEstimation()).gas_estimate;
 
     // Verify assets have come into the wallet of Alice
-    let newBalance = await aptos.account.getAccountAPTAmount(alice);
+    let newBalance = await aptos.account.getAccountAPTAmount(accounts.alice);
     expect(newBalance).toBe(oldBalance + drainAmount - gasCost);
 
     // Verify assets have left the pool
-    const poolObject = await getPoolObject(alice, Tokens.APT);
+    const poolObject = await getPoolObject(accounts.alice, Tokens.APT);
     const availableStoreAddress = poolObject["available"]["store"]["inner"];
     const availableStore = await aptos.getAccountResource({accountAddress: availableStoreAddress, resourceType: "0x1::fungible_asset::FungibleStore"});
     expect(availableStore["balance"]).toBe((poolAmount - drainAmount).toString());
@@ -181,31 +189,20 @@ test("Drain from the pool", async () => {
 test("Excess Drain from the pool", async () => {
     // Create the pool
     let poolAmount = 100;
-    {
-        const transaction = await aptos.transaction.build.simple({
-            sender: alice.accountAddress,
-            data: {
-                function: `${deployerAccount.accountAddress}::streams::create_pool`,
-                functionArguments: [Tokens.APT, poolAmount],
-                typeArguments: ["0x1::fungible_asset::Metadata"],
-            },
-        });
-        const pendingTransaction = await aptos.signAndSubmitTransaction({signer: alice, transaction});
-        await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
-    }
+    await createPool(accounts.alice, poolAmount);
 
     try {
         let drainAmount = poolAmount + 1;
         // Drain the pool
         const transaction = await aptos.transaction.build.simple({
-            sender: alice.accountAddress,
+            sender: accounts.alice.accountAddress,
             data: {
                 function: `${deployerAccount.accountAddress}::streams::drain_pool`,
                 functionArguments: [Tokens.APT, drainAmount],
                 typeArguments: ["0x1::fungible_asset::Metadata"],
             }
         });
-        const pendingTransaction = await aptos.signAndSubmitTransaction({signer: alice, transaction});
+        const pendingTransaction = await aptos.signAndSubmitTransaction({signer: accounts.alice, transaction});
         await aptos.waitForTransaction({transactionHash: pendingTransaction.hash});
         throw new Error("Expected failure of transaction");
     } catch (error: any) {
